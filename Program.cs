07968e071a732fc81fdf128e673978f83a83aeda
ignaciodiazoreiro <72936156+ignaciodiazoreiro@users.noreply.github.com>
2022-05-29 07:29:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
