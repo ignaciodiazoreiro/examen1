639971a48e90b14f5408fa737dcd8ed32d2bbf76
ignaciodiazoreiro <72936156+ignaciodiazoreiro@users.noreply.github.com>
2022-05-29 07:33:02 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Examen_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Hasta Luego!");
         }
     }
 }
